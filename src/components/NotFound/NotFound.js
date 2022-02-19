f8e82e9260a81e8db85abf013220bbcd4de806b9
nima <nimaqanbari1999@gmail.com>
2022-02-19 07:13:31 -0800

    add NotFoundPage

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from "./NotFound.module.css"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div  className={styles.container}>
+      <div>
+      <h3>صفحه ای که به دنبال آن بودید پیدا نشد!</h3>
+      <Link to="/">صفحه اصلی</Link>
+      </div>
+      <div className={styles.img}></div>
+    </div>
   )
 }
 
